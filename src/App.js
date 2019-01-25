0655ae9e16669f100d515c6dfab3d67b4db35b85
Essybern <esberngeilini@gmail.com>
2019-01-25 14:56:06 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+/*import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        data: []
+    };
+
+    // Code is invoked after the component is mounted/inserted into the DOM tree.
+    componentDidMount() {
+        const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Faroe+Islandsg&format=json&origin=*";
+
+        fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    data: result
+                })
+            });
+    }
+
+    render() {
+        const { data } = this.state;
+
+        const result = data.map((entry, index) => {
+            return <li key={index}>{entry}</li>;
+        });
+
+        return <ul>{result}</ul>;
+    }
+}
+
+export default App;
+*/
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component {
+    state = {
+        characters: []  
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    handleSubmit = character => { //Allar vit hava higartil + tann nýggja
+        this.setState({characters: [...this.state.characters, character]});
+    }
+     
+    render() {
+        const { characters } = this.state
+        return (
+            <div className="container">
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
+}
+
+export default App;
